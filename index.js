5bbe1c4cf824e5982c3f420e14de1fce94ea08fc
Catalina Williams <catalina.rose1361@gmail.com>
2020-01-17 18:00:13 -0700

    working inquirer questions

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-var fs = require("fs");
 
+var fs = require("fs");
+var inquirer = require("inquirer");
+// const axios = require("axios");
+
+// code to prompt to user questions
 const questions = [
+    {
+        type: "input",
+        message: "Please type your Github username",
+        name: "username"
+    },
+    {
+        type: "list",
+        message: "what is your favorite color?",
+        name: "favorite color",
+        choices: ["green", "blue", "pink", "red"]
+    }
   
 ];
+// function to take inout from user and use it to get back info from 
+inquirer
+  inquirer.prompt(questions).then(answers => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  });
 
-function writeToFile(fileName, data) {
+
+// async function getUsername() {
+   
+//     const { username } = await inquirer.prompt(questions);
+
+//     const response = await axios.get("https://api.github.com/users/${username}");
+
+
+// }
+
+// create PDF document function
+// edit pdf document
+// function writeToFile(fileName, data) {
  
-}
+// }
 
-function init() {
+// function init() {
 
-init();
+// init();
+// }
